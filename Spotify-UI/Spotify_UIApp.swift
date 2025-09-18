a54bc45fd00aef1213ee934d0c16e565fb50f180
jamesellisunc <jamellis@ad.unc.edu>
2025-09-18 09:48:45 -0400

    Initial Commit

---
+++ Spotify-UI/Spotify_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_UIApp.swift
+//  Spotify-UI
+//
+//  Created by James Ellis on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
